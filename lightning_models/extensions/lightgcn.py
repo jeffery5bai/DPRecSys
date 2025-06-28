bd84000d8d3a21c8f0636bdcdbc795503a1499bc
jeffery5bai <jeffery5bai@gmail.com>
2025-06-28 12:01:06 +0800

    build lightgcn

---
+++ lightning_models/extensions/lightgcn.py
@@ -0,0 +1,166 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+import torch
+from modules.loss import BPRLoss, EmbLoss
+from modules.lightgcn_modules import LightGCN
+from pytorch_lightning import LightningModule
+from common.eval import Evaluator
+
+class LightGCNRec(LightningModule):
+    """
+    Wrap up `LightGCN` model into Lightning Module as base recommendation model.
+    """
+
+    def __init__(
+        self,
+        graph_data,
+        num_users,
+        num_items,
+        embedding_dim=64,
+        num_layers=3,
+        lr=1e-3,
+        reg_weight=1e-5,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.graph_data = graph_data
+
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.lr = lr
+        self.reg_weight = reg_weight
+
+        self.user_emb = None
+        self.item_emb = None
+
+        self.val_step_outputs = []
+        self.test_step_outputs = []
+        self.val_results = dict()
+        self.test_results = dict()
+
+        self.evaluator = Evaluator()
+
+        self.ngcf_model = LightGCN(
+            num_users=self.num_users,
+            num_items=self.num_items,
+            edge_index=self.graph_data.edge_index,
+            embedding_dim=self.embedding_dim,
+            num_layers=self.num_layers,
+        )
+
+        # NOTE: Main loss functions for recommendation
+        self.bpr_loss = BPRLoss(gamma=1e-10)
+        self.reg_loss = EmbLoss(norm=2)
+
+    def forward(self, user_emb, item_emb, user_idx, item_idx):
+        # NOTE: the dimension of user_emb = [num_user + 1, emb_dim], item_emb = [num_item + 1, emb_dim]
+        # two towers dot product
+        return (user_emb[user_idx] * item_emb[item_idx]).sum(dim=1)
+
+    def training_step(self, batch, batch_idx):
+        user_emb, item_emb = self.ngcf_model()  # Compute embedding for training
+
+        user, pos_item, neg_item = batch["user"], batch["pos_item"], batch["neg_item"]
+        yp_scores = self.forward(user_emb, item_emb, user, pos_item)
+        yn_scores = self.forward(user_emb, item_emb, user, neg_item)
+
+        # Main task: BPR (Pair-wise + L2 regularization loss)
+        pair_loss = self.bpr_loss(yp_scores, yn_scores)
+        reg_loss = self.reg_loss(user_emb, item_emb[pos_item], item_emb[neg_item])
+        bpr_loss = pair_loss + self.reg_weight * reg_loss
+        self.log_dict(
+            {
+                "train_bpr_loss": bpr_loss,
+                "train_pair_loss": pair_loss,
+                "train_reg_loss": self.reg_weight * reg_loss,
+            },
+            on_epoch=True,
+            on_step=True,
+        )
+        return bpr_loss
+
+    # NOTE: Validation
+    def on_validation_epoch_start(self):
+        self.user_emb, self.item_emb = self.ngcf_model()  # Compute embedding once for val phase
+
+    def validation_step(self, batch, batch_idx):
+        """We use user-item pairs for inference, so we need to compute scores for each pair."""
+        user, pos_item, neg_item = batch["user"], batch["pos_item"], batch["neg_item"]
+        yp_scores = self.forward(self.user_emb, self.item_emb, user, pos_item)
+        yn_scores = self.forward(self.user_emb, self.item_emb, user, neg_item)
+
+        # Main task: BPR (Pair-wise + L2 regularization loss)
+        pair_loss = self.bpr_loss(yp_scores, yn_scores)
+        reg_loss = self.reg_loss(self.user_emb, self.item_emb[pos_item], self.item_emb[neg_item])
+        bpr_loss = pair_loss + self.reg_weight * reg_loss
+
+        self.log_dict({"val_bpr_loss": bpr_loss}, on_epoch=True, on_step=True)
+
+        self.val_step_outputs.append({"bpr_loss": bpr_loss})
+
+    def on_validation_epoch_end(self):
+        outputs = self.val_step_outputs
+        all_bpr_loss = torch.cat([x["bpr_loss"] for x in outputs])
+
+        self.val_results = {
+            "bpr_loss": all_bpr_loss.cpu(),
+        }
+
+        # self.log_dict({"val_avg_bpr_loss": avg_bpr_loss}, prog_bar=True)
+
+    # NOTE: Testing/Inference
+    def on_test_epoch_start(self):
+        self.user_emb, self.item_emb = self.ngcf_model()
+
+    def test_step(self, batch, batch_idx):
+        """We use user-item pairs for inference, so we need to compute scores for each pair."""
+        user, item, label = batch["user"], batch["item"], batch["label"]
+        scores = self.forward(self.user_emb, self.item_emb, user, item)
+
+        self.test_step_outputs.append(
+            {
+                "users": user,
+                "items": item,
+                "scores": scores,
+                "labels": label,
+            }
+        )
+
+    def on_test_epoch_end(self):
+        outputs = self.test_step_outputs
+        all_users = torch.cat([x["users"] for x in outputs])
+        all_items = torch.cat([x["items"] for x in outputs])
+        all_scores = torch.cat([x["scores"] for x in outputs])
+        all_labels = torch.cat([x["labels"] for x in outputs])
+
+        self.test_results = {
+            "user": all_users.cpu(),
+            "item": all_items.cpu(),
+            "score": all_scores.cpu(),
+            "label": all_labels.cpu(),
+            "user_emb": self.user_emb.detach().cpu(),
+            "item_emb": self.item_emb.detach().cpu(),
+        }
+
+        eval_df = self.evaluator.prepare_evaluation_data(self.test_results)
+        eval_score_df = self.evaluator.evaluate(eval_df, K=5)
+        eval_score_df = self.evaluator.evaluate(eval_score_df, K=10)
+        eval_score_df = self.evaluator.evaluate(eval_score_df, K=20)
+        metrics = {
+            f"test_{metric}{k}": eval_score_df[f"{metric}@{k}"].mean()
+            for metric in ["ndcg", "precision", "recall"]
+            for k in [5, 10, 20]
+        }
+        self.log_dict(metrics, prog_bar=True)
+        self.test_results["eval_score_df"] = eval_score_df
+
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
---
+++ modules/lightgcn_modules.py
@@ -0,0 +1,65 @@
+"""
+LightGCN module for PyTorch Geometric.
+He, Xiangnan, et al. "Lightgcn: Simplifying and powering graph convolution network for recommendation."
+Proceedings of the 43rd International ACM SIGIR conference on research and development in Information Retrieval. 2020.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+
+
+class LightGCN(nn.Module):
+    def __init__(self, edge_index, num_users, num_items, embedding_dim, num_layers):
+        super().__init__()
+        self.register_buffer("edge_index", edge_index)
+        self.edge_index = edge_index
+        self.num_users = num_users + 1  # +1 for OOV
+        self.num_items = num_items + 1  # +1 for OOV
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        # Initialize user and item embeddings
+        self.user_embedding = nn.Embedding(self.num_users, embedding_dim)
+        self.item_embedding = nn.Embedding(self.num_items, embedding_dim)
+        nn.init.xavier_uniform_(self.user_embedding.weight)
+        nn.init.xavier_uniform_(self.item_embedding.weight)
+
+        # Define NGCF layers
+        self.convs = nn.ModuleList()
+        for _ in range(num_layers):
+            self.convs.append(LightGCNConv())
+
+    def forward(self):
+        x = torch.cat([self.user_embedding.weight, self.item_embedding.weight], dim=0)
+        all_embeddings = [x]
+
+        for k, conv in enumerate(self.convs):
+            x = conv(x, self.edge_index)
+            all_embeddings.append(x / (k + 2))
+
+        # Concatenate embeddings from all layers
+        final_embedding = torch.sum(torch.stack(all_embeddings, dim=0), dim=0)
+        user_embeddings = final_embedding[: self.num_users]
+        item_embeddings = final_embedding[self.num_users :]
+        return user_embeddings, item_embeddings
+
+
+class LightGCNConv(MessagePassing):
+    def __init__(self):
+        super().__init__(aggr="add")
+
+    def forward(self, x, edge_index):
+        # Compute symmetric normalization in classic GCN
+        edge_index, edge_weight = gcn_norm(edge_index, add_self_loops=False)
+
+        return self.propagate(edge_index, x=x, edge_weight=edge_weight)
+
+    def message(self, x_j, edge_weight):
+        # Propagate normalized messages
+        return edge_weight.view(-1, 1) * x_j
+
+    def update(self, aggr_out):
+        return F.normalize(aggr_out, p=2, dim=1)
